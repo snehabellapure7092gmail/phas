a1686825a2bc767849df8dd9b659063fa2bb9cfc
snehabellapure7092gmail <98867302+snehabellapure7092gmail@users.noreply.github.com>
2022-02-05 12:44:56 +0530

    Add files via upload

---
+++ lesson2/innerClassAssisted3.java
@@ -0,0 +1,21 @@
+package lesson2;
+
+
+public class innerClassAssisted3 {
+
+	public static void main(String[] args) {
+		
+		
+	AnonymousInnerClass i = new AnonymousInnerClass() {
+
+	         public void display() {
+	        	 
+	        	 
+	            System.out.println("Anonymous Inner Class");
+	         }
+	      };
+	      i.display();
+	   }
+	}
+
+
